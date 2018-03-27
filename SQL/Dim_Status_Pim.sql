6dc1d3ac8a253db4e1c843f9aed777690e80ef24
Pim <pdemeulemeester@kvl.nl>
2018-03-27 10:34:13 +0200

    SQL met Checks!

--- SQL/Dim_Status_Pim.sql
+++ SQL/Dim_Status_Pim.sql
@@ -9,3 +9,33 @@ Create Table DimStatus (
 );
 
 Drop Table DimStatus;
+
+Truncate Table Dimstatus;
+
+use Cursus
+
+Update  [Cursus].dbo.[Deelname] set  [Status] = 'Geen zin in' WHERE [DeelnameId] = 280
+Update  [Cursus].dbo.[Deelname] set  [Status] = 'Deelgenomen' WHERE [DeelnameId] = 280
+
+-- test constraints per column
+
+use DWH
+
+--DimKey
+Update  dbo.DimStatus set  DimKey = 2 WHERE [DimKey] = 1 -- test uniciteit van waarden
+SELECT COUNT([DimKey]), COUNT(DISTINCT([DimKey])) from dbo.DimStatus -- test not null
+
+--DeelnameId
+SELECT COUNT(DeelnameId), COUNT(DISTINCT(DeelnameId)) from dbo.DimStatus -- test not null
+--Status
+SELECT COUNT([Status]), COUNT(DISTINCT(Status)) from dbo.DimStatus -- test not null
+--Checksum
+SELECT COUNT([Checksum]), COUNT(DISTINCT([Checksum])) from dbo.DimStatus -- test not null
+--ValidUntil
+SELECT COUNT([ValidUntil]), COUNT(DISTINCT([ValidUntil])) from dbo.DimStatus -- test not null
+--ValidFrom
+SELECT COUNT([ValidFrom]), COUNT(DISTINCT([ValidFrom])) from dbo.DimStatus -- test not null
+--CurrentFlag
+SELECT COUNT([CurrentFlag]), COUNT(DISTINCT([CurrentFlag])) from dbo.DimStatus -- test not null
+
+select * from  dbo.DimStatus
