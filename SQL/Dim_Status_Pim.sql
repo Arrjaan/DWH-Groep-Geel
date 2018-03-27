fe54855e6e9ac8c827e835017bcec69028c3d990
Pim <pdemeulemeester@kvl.nl>
2018-03-27 10:07:21 +0200

    DimStatus SSIS en SQL DimStatus
    Allerlaatste versie Sql

--- SQL/Dim_Status_Pim.sql
+++ SQL/Dim_Status_Pim.sql
@@ -1,10 +1,10 @@
 Create Table DimStatus (
 [DeelnameId] [int] NOT NULL,
 [DimKey] [bigint] IDENTITY(1,1) NOT NULL,
-[Status] nvarchar(16) NOT NULL,
+[Status] varchar(16) NOT NULL,
 [Checksum] [int] NOT NULL,
 [ValidFrom] [datetime] NOT NULL default Getdate(),
-[ValidUntill] [datetime] NOT NULL default '9999-12-31',
+[ValidUntil] [datetime] NOT NULL default '9999-12-31',
 [CurrentFlag] [bit] NOT NULL default '1'
 );
 
