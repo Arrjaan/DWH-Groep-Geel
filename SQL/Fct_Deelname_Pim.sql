aedf7068f407c8ef475a057e0b022fa2108f4ae9
Pim <pdemeulemeester@kvl.nl>
2018-03-29 09:35:45 +0200

    sql

--- SQL/Fct_Deelname_Pim.sql
+++ SQL/Fct_Deelname_Pim.sql
@@ -1,11 +1,18 @@
 Create Table FctDeelname (
-[DeelnameId] int NOT NULL,
-[CursusId] int NOT NULL,
-[CursistId] int NOT NULL,
+[FctKey] [bigint] IDENTITY(1,1) NOT NULL,
+[DeelnameId] int NOT NULL Primary Key,
 [Bijzonderheden] varchar(400) NULL,
-[Status] varchar(16) NOT NULL,
+[DimKey_Status] int NOT NULL,
+[DimKey_Persoon] int NOT NULL,
+[DimKey_Cursus] int NOT NULL,
 [Checksum] [int] NOT NULL,
 [ValidFrom] [datetime] NOT NULL default Getdate(),
 [ValidUntil] [datetime] NOT NULL default '9999-12-31',
 [CurrentFlag] [bit] NOT NULL default '1'
 );
+
+Drop table dbo.FctDeelname;
+
+Select * From FctDeelname Where DeelnameId = 266;
+
+Truncate Table FctDeelname;
